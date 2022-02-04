4ea92a75ac2aecd7ccd3577facbcfd7ab8e34b1a
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-04 18:45:07 +0100

    added method to write email in DOM

--- js/script.js
+++ js/script.js
@@ -6,22 +6,32 @@ Vue.config.devtools = true;
 const root = new Vue({
   el: "#root",
   data: {
-     emailList: [],
-     rangeList: 10,
+    emailList: [],
+    rangeList: 10,
   },
   methods: {
     getEmail() {
       axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then((res) => {
-           this.emailList.push(res.data.response);
+          this.emailList.push(res.data.response);
+          console.log(this.emailList);
         });
     },
-     getListEmail(array, maxNumber) {
-        do {
-           this.getEmail();
-        } while (array <= maxNumber);
+    getListEmail(number) {
+      for (let i = 0; i < number; i++) {
+        this.getEmail();
+      }
     },
-   },
-  
+  },
+  mounted() {
+    this.getListEmail(this.rangeList);
+  },
 });
+
+//     getListEmail(array, maxNumber) {
+//       do {
+//         this.getEmail();
+//       } while (array.length < maxNumber);
+//     },
+//   },
