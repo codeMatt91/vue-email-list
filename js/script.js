c29b8adb29624666f68570a174232e8c6282db70
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-04 17:51:59 +0100

    added data in script js + methods

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('JS OK!')
+console.log("VUEJS OK!", Vue);
+console.log("AXIOS OK", axios);
+
+Vue.config.devtools = true;
+
+const root = new Vue({
+  el: "#root",
+  data: {
+    emailList: [],
+  },
+  methods: {
+    getEmail() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((res) => {
+          console.log(res.data);
+        });
+    },
+    created() {
+      this.getEmail();
+    },
+  },
+});
